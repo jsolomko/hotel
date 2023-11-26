adb00de9ad3aa85fcfceec589b9f16a80f59d096
Eugene <jsolomko@bk.ru>
2023-11-26 17:33:17 +0300

    -

--- app/src/main/java/com/example/hotelapplication/app/MainActivity.kt
+++ app/src/main/java/com/example/hotelapplication/app/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.hotelapplication.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.hotelapplication.R
 import com.example.hotelapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    var navController: NavController? = null
+    private var appBarConfiguration: AppBarConfiguration? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,8 +22,19 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
 
+        navController = navHostFragment.navController
+        appBarConfiguration =
+            AppBarConfiguration(
+                setOf(
+                    R.id.hotelFragment
+                )
+            )
+        
+        setupActionBarWithNavController(navController!!, appBarConfiguration!!)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController?.navigateUp() ?: super.onSupportNavigateUp()
     }
 }
